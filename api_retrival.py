f0261c21815f0deea2d0b1c8e4d82be1ecf7cfbc
GerryPidgeon <gerry.pidgeon@tastyurban.com>
2024-04-13 17:54:24 +0100

    First Commit

--- api_retrival.py
+++ api_retrival.py
@@ -5,7 +5,7 @@ st.title('Hello Streamlit!')
 st.write("This is a simple test to check if Streamlit is working.")
 
 # Constants for Bungie's OAuth
-CLIENT_ID = '40227'  # Your Bungie application client ID
+CLIENT_ID = '46615'  # Your Bungie application client ID
 REDIRECT_URI = 'https://destinyvaulttool.streamlit.app/callback'  # Your callback URL registered in the Bungie application
 AUTH_URL = 'https://www.bungie.net/en/OAuth/Authorize'
 TOKEN_URL = 'https://www.bungie.net/platform/app/oauth/token/'
