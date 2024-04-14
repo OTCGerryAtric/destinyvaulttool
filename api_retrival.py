88ebea122d4d99dff1bceaecfe5b706c39f35348
GerryPidgeon <gerry.pidgeon@tastyurban.com>
2024-04-14 19:49:29 +0100

    First Commit

--- api_retrival.py
+++ api_retrival.py
@@ -18,9 +18,7 @@ def generate_auth_url():
 
 if st.button('Login with Bungie.net'):
     auth_url = generate_auth_url()
-    # Redirect in the same window
-    js_redirect = f"window.location.href = '{auth_url}';"
-    st.components.v1.html(f"<script>{js_redirect}</script>", height=100)
+    st.markdown(f"Please [click here to authenticate]({auth_url}) with Bungie.net.")
 
 def exchange_code_for_token(code):
     data = {
